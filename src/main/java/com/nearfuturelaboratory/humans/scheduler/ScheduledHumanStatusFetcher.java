21e9a92c04baedff6b1a8c3da156a000dc098f0f
Julian Bleecker <julian@nearfuturelaboratory.com>
2014-02-11 01:23:44 -0800

    This is meant to update the status of a single human. Typically, one would not run this as a repeating job, but one-off. Basically run it once when a new human is created to make status available asap.

---
+++ src/main/java/com/nearfuturelaboratory/humans/scheduler/ScheduledHumanStatusFetcher.java
@@ -0,0 +1,44 @@
+package com.nearfuturelaboratory.humans.scheduler;
+
+import com.nearfuturelaboratory.humans.dao.HumansUserDAO;
+import com.nearfuturelaboratory.humans.entities.Human;
+import com.nearfuturelaboratory.humans.entities.HumansUser;
+import org.quartz.Job;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * Created by julian on 2/9/14.
+ */
+
+/**
+ * Takes a humanid and access_token in order to run as a job a fetch of human status and then caching of it.
+ * A way to immediately update a human's status for, example, when you create a new human so you don't have to
+ * wait a full refresh cycle..
+ * @see com.nearfuturelaboratory.humans.rest.UserHandler#updateStatusAndCacheForHuman(String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+ */
+public class ScheduledHumanStatusFetcher implements Job {
+
+    final static Logger logger = LogManager.getLogger(ScheduledHumanStatusFetcher.class);
+
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobDataMap data = context.getJobDetail().getJobDataMap();
+        String human_id = data.getString("humanid");
+        String access_token = data.getString("access_token");
+        logger.info("Fetch Individual Human Status for humanid="+human_id+" access_token="+access_token+" context="+context);
+        HumansUserDAO dao = new HumansUserDAO();
+        HumansUser user = dao.findOneByAccessToken(access_token);
+        if(user != null) {
+            logger.info("Found user="+user);
+            Human human = user.getHumanByID(human_id);
+            user.serviceRefreshStatusForHuman(human);
+            user.refreshCache(human);
+        }
+    }
+
+
+
+}
